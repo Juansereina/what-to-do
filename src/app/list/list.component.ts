e18d1e091a66d705dc036d340eb57706102c5c9f
Juansereina <juan-2296@hotmail.es>
2018-06-15 13:24:26 -0500

    loading todo lists

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { ITodo } from '../structures/todos';
+import { Observable } from 'rxjs';
+import { TodoService } from './../services/todos.service';
+
 @Component({
   selector: 'list',
   templateUrl: './list.component.html'
 })
 export class ListComponent implements OnInit {
   public listId: string;
-  constructor(private route: ActivatedRoute) {}
+  public todos: Observable<ITodo[]>;
+  constructor(private route: ActivatedRoute, private todoS: TodoService) {}
   ngOnInit() {
     this.listId = this.route.snapshot.params.id;
+    this.todos = this.todoS.getFromList(this.listId);
   }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
-  AngularFirestoreCollection
+  AngularFirestoreCollection,
+  DocumentChangeAction
 } from 'angularfire2/firestore';
 
 import { ITodo } from '../structures/todos';
 
 import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable()
 export class TodoService {
   private collection: AngularFirestoreCollection<ITodo>;
   private listId: string;
+  private ref: Observable<DocumentChangeAction<ITodo>[]>;
 
   constructor(private afs: AngularFirestore) {}
 
@@ -20,6 +24,20 @@ export class TodoService {
       .collection('lists')
       .doc(listId)
       .collection('todos');
+    this.ref = this.collection.snapshotChanges();
+  }
+
+  getFromList(listId: string): Observable<ITodo[]> {
+    if (!this.collection || this.listId !== listId) { this.setCollection(listId); }
+    return this.ref.pipe(
+      map(actions => {
+        return actions.map(item => {
+          const data = item.payload.doc.data() as ITodo;
+          const id = item.payload.doc.id;
+          return { ...data, id };
+        });
+      })
+    );
   }
   add(listId: string, todo: ITodo): Promise<any> {
     if (!this.collection || this.listId !== listId) {
