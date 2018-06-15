64d5264ca08e9533c64b117a626c889de41fd16c
Juansereina <juan-2296@hotmail.es>
2018-06-15 15:40:55 -0500

    animation for todoItems

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService } from './../services/list.service';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  animations: [
+    trigger('enterState', [
+      state(
+        'void',
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0
+        })
+      ),
+      transition(':enter', [
+        animate(
+          300,
+          style({
+            transform: 'translateX(0)',
+            opacity: 1
+          })
+        )
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   public message: string;
