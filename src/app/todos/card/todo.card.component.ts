714a453ef07006a8caada2ec3ef11c400ff57726
Juansereina <juan-2296@hotmail.es>
2018-06-16 22:02:48 -0500

    animation for press items

--- src/app/todos/card/todo.card.component.ts
+++ src/app/todos/card/todo.card.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ITodo } from '../../structures/todos';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'todo-card',
-  templateUrl: './todo.card.component.html'
+  templateUrl: './todo.card.component.html',
+  animations: [
+    trigger('pressAnimation', [
+      state('up, void', style({
+        transform: 'translateX(0)'
+      })),
+      state('down', style({
+        transform: 'translateX(-100px)'
+      })),
+      transition('up <=> down', [
+        animate(100, style({ transform: 'translateX(0)'})),
+        animate(100)
+      ])
+    ])
+  ]
 })
 export class TodoCardComponent implements OnInit {
   @Input() todo: ITodo;
+  public press: 'up';
   constructor() {}
   ngOnInit() {}
   completed() {
